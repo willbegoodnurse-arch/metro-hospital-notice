33b73995616c816bfde8b89702eb1b9ff65903a3
Metro Hospital <willbegoodnurse@gmail.com>
2026-04-09 21:35:11 +0900

    feat: 상단 타이틀을 중환자실 공지사항으로 변경
    - Header: 부서 공지사항 → 중환자실 공지사항 (ICU Notice Board)
    - index.html: 페이지 title 및 description 업데이트

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,8 +6,8 @@ export default function Header() {
       <div className="header-inner">
         <img src={logoImg} alt="메트로병원" className="header-logo" />
         <div className="header-title">
-          <h1>부서 공지사항</h1>
-          <span>Department Notice Board</span>
+          <h1>중환자실 공지사항</h1>
+          <span>ICU Notice Board</span>
         </div>
       </div>
     </header>
